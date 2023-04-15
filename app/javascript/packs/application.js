faddb5a20362cc6d3574c4e93395e07f1229aebf
hendrik-ubuntu <li.zhiqiang2011@gmail.com>
2023-04-15 14:34:40 +0700

    add navigation menu

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,8 +5,10 @@
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
+import jQuery from "jquery"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import '@doabit/semantic-ui-sass'
 
 Rails.start()
 Turbolinks.start()
