c5585b7aa7c25358981da1e8c9a948b187a5a610
hendrik-ubuntu <li.zhiqiang2011@gmail.com>
2023-04-15 18:31:39 +0700

    add gem bcrypt, add model user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: "shia_labeouf", password: "password123")
+User.create(username: "john_wick", password: "password123")
+User.create(username: "rachel_weisz", password: "password123")
+User.create(username: "peter_stormare", password: "password123")
+User.create(username: "tilda_swinton", password: "password123")
