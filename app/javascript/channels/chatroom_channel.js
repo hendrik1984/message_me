08e749b3ec00cb4f18f5be7291fee2ae93971331
hendrik-ubuntu <li.zhiqiang2011@gmail.com>
2023-04-26 17:08:34 +0700

    add action cable in controller message

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -3,6 +3,7 @@ import consumer from "./consumer"
 consumer.subscriptions.create("ChatroomChannel", {
   connected() {
     // Called when the subscription is ready for use on the server
+    console.log("Connected to chatroom...")
   },
 
   disconnected() {
@@ -11,5 +12,6 @@ consumer.subscriptions.create("ChatroomChannel", {
 
   received(data) {
     // Called when there's incoming data on the websocket for this channel
+    console.log(data.foo);
   }
 });
